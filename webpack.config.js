e531b51d5386e12af88f34454a6dda61c009f72f
Shaher Jamal Eddin <JAS6MU@bosch.com>
2024-12-23 13:20:31 +0100

    Add plugin and use it to generate the HTML in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,4 +14,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
